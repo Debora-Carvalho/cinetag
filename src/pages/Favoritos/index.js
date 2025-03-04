e372808a40dac6497da2496ab8d41633e8499dc9
Debora-Carvalho <deboravolc2@gmail.com>
2025-03-03 21:28:22 -0300

    finalizando funções para a pagina de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -2,14 +2,19 @@ import styles from './Favoritos.module.css';
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
+import { useFavoritoContext } from 'contextos/Favoritos';
 
 function Favoritos() {
+    const { favorito } = useFavoritoContext();
+
     return (
         <>
             <Banner imagem="favoritos"></Banner>
             <Titulo>Meus Favoritos</Titulo>
             <section className={styles.container}>
-                <Card id='2' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png'/>           
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
             </section>
         </>
     )
