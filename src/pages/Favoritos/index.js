bee45cd521c3b5c8d0832c3904122bcb8e0388a0
Debora-Carvalho <deboravolc2@gmail.com>
2025-03-03 21:18:36 -0300

    add função para favoritar/ desfavoritar videos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,16 @@
 import styles from './Favoritos.module.css';
+import Banner from "components/Banner";
+import Card from "components/Card";
+import Titulo from "components/Titulo";
 
 function Favoritos() {
     return (
         <>
+            <Banner imagem="favoritos"></Banner>
+            <Titulo>Meus Favoritos</Titulo>
+            <section className={styles.container}>
+                <Card id='2' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png'/>           
+            </section>
         </>
     )
 }
