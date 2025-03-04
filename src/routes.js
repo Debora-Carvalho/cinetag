44964ff996e60cb3605fda9b883abd179e26e693
Debora-Carvalho <deboravolc2@gmail.com>
2025-03-03 22:52:47 -0300

    add paginas Player e erro 404

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "pages/Inicio";
 import Favoritos from "pages/Favoritos";
+import Player from "pages/Player";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
 import Container from "components/Container";
 import FavoritosProvider from "contextos/Favoritos";
+import NaoEncontrada from "pages/NaoEncontrada";
 
 function AppRoutes() {
     return(
@@ -15,6 +17,8 @@ function AppRoutes() {
                     <Routes>
                         <Route path="/" element={<Inicio />}></Route>
                         <Route path="/favoritos" element={<Favoritos />}></Route>
+                        <Route path="/:id" element={<Player />}></Route>
+                        <Route path="*" element={<NaoEncontrada />}></Route>
                     </Routes>
                 </FavoritosProvider>
             </Container>
